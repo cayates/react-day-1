59bcfbeee851c10dedebed1601e3360a07f9d395
Chad Yates <cayates1989@gmail.com>
2017-09-13 20:35:11 -0400

    another commit with everything consoling as a success

--- main.js
+++ main.js
@@ -7,7 +7,17 @@
 //            /____/
 
 // Dog Constructor & Prototype
-function Dog () {}
+function Dog (name, status, color, hungry) {
+    'use strict';
+    this.name = name
+    this.status = status
+    this.color = color
+    this.hungry = hungry
+}
+
+const sadie = new Dog ("does not matter", "normal", "black", false)
+const moonshine = new Dog ("does not matter", "does not matter", "does not matter", true)
+const atticus = new Dog ("does not matter", "does not matter", "does not matter", true)
 
 // Instances of Dog
 // Needed: sadie, moonshine, atticus
@@ -19,7 +29,22 @@ function Dog () {}
 // /_/ /_/\__,_/_/ /_/ /_/\__,_/_/ /_/____/
 
 // Human Constructor & Prototype
-function Human () {}
+function Human (name, cool) {
+    'use strict';
+    this.name = name
+    this.cool = cool
+}
+
+Human.prototype.pet = function(doggies){
+    doggies.status = "happy"
+  }
+  
+Human.prototype.feed = function(doggies){
+    doggies.hungry = false;
+  }
+
+const mason = new Human("does not matter", false)
+const julia = new Human("does not matter", true)
 
 // Instances of Human
 // Needed: mason, julia
